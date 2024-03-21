345a6a761a2a2a1a9decb16ec91768cd33d45baa
henryye <yixuy@sfu.ca>
2024-03-21 00:36:16 -0400

    Update write the data csv function

--- Dataset/lbank.py
+++ Dataset/lbank.py
@@ -1,37 +1,28 @@
 import ccxt
-import requests
-import json
 import csv
-import time
-import pyxt
 
-# exchange platform
-print(ccxt.exchanges)
-# choose exchange platform
-exchange_symbol = "doge_usdt"
-exchange = ccxt.lbank()
-btc_usdt = exchange.fetch_trades(exchange_symbol)
+# Fieldnames or column headers in the CSV file
+field_names = {'timestamp', 'datetime', 'symbol', 'type', 'price',  'amount', 'cost' }
 
-
-# t_end = time.time() + 1*5
-res = []
-# while time.time() < t_end:
-res += exchange.fetch_trades(exchange_symbol)
+def get_data(platform_name, exchange_symbol):
+    exchange = ''
+    if platform_name == 'lbank':
+        exchange = ccxt.lbank()
+    res = exchange.fetch_trades(exchange_symbol)
+    return [{k: v for k, v in row.items() if k in field_names} for row in res]
 
 # Specify the CSV file name
-filename = f'data/{exchange_symbol}_transaction.csv'
-# Fieldnames or column headers in the CSV file
+def exchange_save_csv(platform_name, exchange_symbol):
+    filename = f'doge_data/{platform_name}_{exchange_symbol}_transaction.csv' if exchange_symbol == 'doge_usdt'                                        else f'shiba_data/{platform_name}_{exchange_symbol}_transaction.csv'
+    # Writing to CSV
+    filtered_data = get_data(platform_name, exchange_symbol)
+    with open(filename, 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, field_names)
+        dict_writer.writeheader()
+        dict_writer.writerows(filtered_data)
 
-fieldnames = res[0].keys()
-fieldnames = {'timestamp', 'datetime', 'symbol', 'type', 'price',  'amount', 'cost' }
-
-filtered_data = [{k: v for k, v in row.items() if k in fieldnames} for row in res]
-
-# Writing to CSV
-with open(filename, 'w', newline='') as output_file:
-    dict_writer = csv.DictWriter(output_file, fieldnames)
-    dict_writer.writeheader()
-    dict_writer.writerows(filtered_data)
+exchange_save_csv("lbank", "doge_usdt")
+exchange_save_csv("lbank", "shib_usdt")
 
 
 
