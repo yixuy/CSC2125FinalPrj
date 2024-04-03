603b3099975c831ef6dba64580c313bf7ba113ca
Yizhou Shen <yizhou.shen@outlook.com>
2024-04-03 09:22:10 -0400

    simulate market time

--- Simulator/app/main.py
+++ Simulator/app/main.py
@@ -6,36 +6,62 @@ import time
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
+def getMarketStartTime():
+    lbank_shib_path = os.path.join(current_dir, r'static\shib_data\lbank_shib_transaction.csv')
+    with open(lbank_shib_path, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        next(csv_reader)
+        for row in csv_reader:
+            timestamp = int(row['timestamp'])
+            if timestamp > 0:
+                return timestamp
+
+market_start_timestamp = getMarketStartTime()
+print("market start timestamp: ", market_start_timestamp)
+real_start_timestamp = round(time.time() * 1000)
+print("real start timestamp: ", real_start_timestamp)
+
 @marketsim.route('/',methods=['GET'])
 def main():
     return 'Hello, World'
 
 @marketsim.route('/getPrice',methods=['POST'])
 def getPrice():
+    if 'exchange' not in request.form:
+        return "Need an exchange market"
+    if 'cryptocurrency' not in request.form:
+        return "Need a cryptocurrency"
     exchange = request.form.get('exchange')
     cryptocurrency = request.form.get('cryptocurrency')
     csv_file_path = os.path.join(current_dir, r'static\{}_data\{}_{}_transaction.csv'.format(cryptocurrency, exchange, cryptocurrency))
     print("csv path: ", csv_file_path)
     
-    target_timestamp = 1712135713355
-    start_time = time.time_ns()
+    real_current_timestamp = round(time.time() * 1000)
+    print("real current timestamp: ", real_current_timestamp)
+    elapsed_time = real_current_timestamp - real_start_timestamp
+    market_target_timestamp = market_start_timestamp + elapsed_time
+    print("market target timestamp: ", market_target_timestamp)
+    
+    # start_time = time.time() * 1000
+    start_time_micro = time.time_ns() // 1000
 
     with open(csv_file_path, "r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             timestamp = int(row['timestamp'])
-            if timestamp == target_timestamp:
+            if timestamp >= market_target_timestamp:
                 price = float(row['price'])
-                print("Price at timestamp {}: {}".format(target_timestamp, price))
+                print("Price at timestamp {}: {}".format(market_target_timestamp, price))
                 break
         else:
-            print("Timestamp {} not found in the CSV file.".format(target_timestamp))
+            print("Timestamp {} not found in the CSV file.".format(market_target_timestamp))
 
-    end_time = time.time_ns()
+    # end_time = time.time() * 1000
+    end_time_micro = time.time_ns() // 1000
 
     # Calculate the elapsed time
-    elapsed_time = end_time - start_time
-    print("Elapsed time:", elapsed_time, "nanoseconds")
+    time_to_retrieve_price = end_time_micro - start_time_micro
+    print("time to retrieve price:", time_to_retrieve_price, "microseconds")
 
     response = jsonify(success='True', price=price)
     return response
