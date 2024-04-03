686cb22696530ad20b04b9b7e89144b637e870d2
Bragi Xia <xyqsun00@gmail.com>
2024-04-02 22:14:44 -0400

    Add template for simulator and model

---
+++ Dataset/app/app.py
@@ -0,0 +1,24 @@
+'''
+Write multithreaded listener for price
+'''
+import time
+
+if __name__ == "__main__":
+
+    virtual_beginning_time = 0 # set to a timestamp
+    actual_beginning_time = time.time()
+
+
+    # Set expected latency in second
+    expected_latency = 0.5
+
+    while time.time() - actual_beginning_time < 5 * 60 * 60: # set to 5hrs of simulation time
+        '''
+        We want multiple threads listening to all exchanges
+        By listening it means sending request (timestamp, cointype, exchange_name) to simulator 
+        '''
+
+        # Assume p1 is price of doge on ex1, p2 is price of doge on ex2. Then:
+
+        # if p1 and p2 satisfies our model, then make transaction
+        pass
