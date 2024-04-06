ead763717f2b826d3e2ed25f6b23e0dbfc123f8a
henryye <yixuy@sfu.ca>
2024-04-06 11:23:37 -0400

    Change back to 5000

--- Simulator/run.py
+++ Simulator/run.py
@@ -3,4 +3,4 @@ from app import marketsim
 # marketsim.run('0.0.0.0',5000,debug=True)
 
 if __name__ == '__main__':
-    marketsim.run('0.0.0.0', 8080, debug=True)
+    marketsim.run('0.0.0.0', 5000, debug=True)
