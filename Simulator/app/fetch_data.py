09443f78a4c4eb6a46289e4f673e5aafdf27ee17
Yizhou Shen <yizhou.shen@outlook.com>
2024-04-08 18:12:39 -0400

    fetch ohlcv data for variance calculation

--- Simulator/app/fetch_data.py
+++ Simulator/app/fetch_data.py
@@ -125,7 +125,7 @@ def get_market_data(exchange, exchange_symbol: str, save_file: str, time_since: 
 
 def get_market_data_ohlcv(exchange, exchange_symbol: str, save_file: str, time_since: int):
     
-    res = exchange.fetch_ohlcv(exchange_symbol, '1m', since=time_since, limit=600)
+    res = exchange.fetch_ohlcv(exchange_symbol, '1m', since=time_since, limit=1500)
 
     filename = save_file
     header = ['date', 'open', 'high', 'low', 'close', 'volume']
@@ -156,72 +156,71 @@ def get_order_books(exchange, exchange_symbol, limit, save_file: str):
 
 if __name__ == "__main__":
 
-    # default_time_until = int(time.time()) * 1000
-    # default_time_since = default_time_until - 3600 * 1000 * 5 # 5hr data by default
+    time_now = int(time.time()) * 1000
+    time_since = time_now - 3600 * 1000 * 24 # 24 hr data by default
+
+    for market in market_list:
+        print(f"Fetching data for {market['name']}...")
+        # Get Doge Data
+        save_file = f"./static/ohlcvs/doge_data/{market['name']}_doge_transaction.csv"
+        get_market_data_ohlcv(market["exchange"], market["doge_symbol"], save_file, time_since)
+        # Get Shib Data
+        save_file = f"./static/ohlcvs/shib_data/{market['name']}_shib_transaction.csv"
+        get_market_data_ohlcv(market["exchange"], market["shib_symbol"], save_file, time_since)
+    
+    
+    # threads = []
+    # for market in market_list:
+    #     if market['name'] == 'bitstamp': # limit argument does not work on bitstamp
+    #         continue
+    #     print(f"Fetching data for {market['name']}...")
+    #     limit = 20 if market["name"] == "kucoin" else 10 # kucoin only accept limit = 20 or 100
+        
+    #     save_file_doge = f"./static/orders/doge_data/{market['name']}_doge_orderbooks.pkl"
+    #     save_file_shib = f"./static/orders/shib_data/{market['name']}_shib_orderbooks.pkl"
+        
+    #     t1 = threading.Thread(target=get_order_books, args=(market["exchange"], market["doge_symbol"], limit, save_file_doge))
+    #     t2 = threading.Thread(target=get_order_books, args=(market["exchange"], market["shib_symbol"], limit, save_file_shib))
+    #     t1.start()
+    #     t2.start()
+    #     threads.append(t1)
+    #     threads.append(t2)
+    
+    # for thread in threads:
+    #     thread.join()
+    
+    # print("all threads joined")
 
     # for market in market_list:
-    #     time_since = default_time_since
-    #     print(f"Fetching data for {market['name']}...")
-    #     # Get Doge Data
-    #     save_file = f"./static/ohlcvs/doge_data/{market['name']}_doge_transaction.csv"
-    #     get_market_data_ohlcv(market["exchange"], market["doge_symbol"], save_file, time_since)
-    #     # Get Shib Data
-    #     save_file = f"./static/ohlcvs/shib_data/{market['name']}_shib_transaction.csv"
-    #     get_market_data_ohlcv(market["exchange"], market["shib_symbol"], save_file, time_since)
-    
-    
-    threads = []
-    for market in market_list:
-        if market['name'] == 'bitstamp': # limit argument does not work on bitstamp
-            continue
-        print(f"Fetching data for {market['name']}...")
-        limit = 20 if market["name"] == "kucoin" else 10 # kucoin only accept limit = 20 or 100
+    #     if market['name'] == 'bitstamp': # limit argument does not work on bitstamp
+    #         continue
         
-        save_file_doge = f"./static/orders/doge_data/{market['name']}_doge_orderbooks.pkl"
-        save_file_shib = f"./static/orders/shib_data/{market['name']}_shib_orderbooks.pkl"
-        
-        t1 = threading.Thread(target=get_order_books, args=(market["exchange"], market["doge_symbol"], limit, save_file_doge))
-        t2 = threading.Thread(target=get_order_books, args=(market["exchange"], market["shib_symbol"], limit, save_file_shib))
-        t1.start()
-        t2.start()
-        threads.append(t1)
-        threads.append(t2)
-    
-    for thread in threads:
-        thread.join()
-    
-    print("all threads joined")
+    #     print(f"printing out {market['name']} doge data.....")
+    #     order_books_doge = []
+    #     count_doge = 0
+    #     with open(f"./static/orders/doge_data/{market['name']}_doge_orderbooks.pkl", "rb") as f:
+    #         try:
+    #             while True:
+    #                 order_book = pickle.load(f)
+    #                 order_books_doge.append(order_book)
+    #                 count_doge += 1
+    #         except EOFError:
+    #             pass  # Reached end of file
+    #     print(order_books_doge[0])
+    #     print(order_books_doge[-1])
+    #     print(count_doge)
 
-    for market in market_list:
-        if market['name'] == 'bitstamp': # limit argument does not work on bitstamp
-            continue
-        
-        print(f"printing out {market['name']} doge data.....")
-        order_books_doge = []
-        count_doge = 0
-        with open(f"./static/orders/doge_data/{market['name']}_doge_orderbooks.pkl", "rb") as f:
-            try:
-                while True:
-                    order_book = pickle.load(f)
-                    order_books_doge.append(order_book)
-                    count_doge += 1
-            except EOFError:
-                pass  # Reached end of file
-        print(order_books_doge[0])
-        print(order_books_doge[-1])
-        print(count_doge)
-
-        print(f"printing out {market['name']} shib data.....")
-        order_books_shib = []
-        count_shib = 0
-        with open(f"./static/orders/shib_data/{market['name']}_shib_orderbooks.pkl", "rb") as f:
-            try:
-                while True:
-                    order_book = pickle.load(f)
-                    order_books_shib.append(order_book)
-                    count_shib += 1
-            except EOFError:
-                pass  # Reached end of file
-        print(order_books_shib[0])
-        print(order_books_shib[-1])
-        print(count_shib)
+    #     print(f"printing out {market['name']} shib data.....")
+    #     order_books_shib = []
+    #     count_shib = 0
+    #     with open(f"./static/orders/shib_data/{market['name']}_shib_orderbooks.pkl", "rb") as f:
+    #         try:
+    #             while True:
+    #                 order_book = pickle.load(f)
+    #                 order_books_shib.append(order_book)
+    #                 count_shib += 1
+    #         except EOFError:
+    #             pass  # Reached end of file
+    #     print(order_books_shib[0])
+    #     print(order_books_shib[-1])
+    #     print(count_shib)
